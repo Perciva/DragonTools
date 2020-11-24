9c9c39e5ac6387430ae08b52c49cee7d2b9452f0
EternalBeats <sugiarta.wijaya@binus.ac.id>
2020-11-24 09:20:15 +0700

    Added frontend template

---
+++ apps/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Index
+@app.route('/')
+def index():
+	return render_template('index.html', title='Dashboard')
+
+
+# Form stuff
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST' and request.form['submit'] == 'Login':
+		return request.form['email'] + " " + request.form['password']
+	else:
+		return render_template('login.html', title='Login')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if request.method == 'POST' and request.form['submit'] == 'Register':
+		return request.form['firstName'] + " " + request.form['lastName'] + " " + request.form['email'] + " " + request.form['password']+ " " + request.form['confirmPassword']
+	else:
+		return render_template('register.html', title='Register')
+
+@app.route('/forgotPassword', methods=['GET', 'POST'])
+def forgotPassword():
+	if request.method == 'POST' and request.form['submit'] == 'forgotPassword':
+		return request.form['email']
+	else:
+		return render_template('forgot-password.html', title='Forgot Password')
+
+# Error
+@app.route('/404')
+def page404():
+	return render_template('404.html', title='404')
+
+if __name__ == "__main__":
+	app.run(debug=True)
