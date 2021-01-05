4b44200a2a3a80c306cb033ac627292ae765323b
kevindoubleu <kevin.wibamanto@gmail.com>
2021-01-05 21:50:47 +0700

    add virustotal hotfix

--- lib/general.py
+++ lib/general.py
@@ -39,24 +39,27 @@ def foremost(fname):
 
 
 def virustotalCheck(fname):
-	sha256sum = sha256(fname)
-	url = 'https://www.virustotal.com/vtapi/v2/file/report'
-	params = {'apikey': '6201732de559c1e9c089e897ce858f0df21efb98f417e51c5ec6a08031abcf6e', 'resource': sha256sum}
-	response = requests.get(url, params=params)
-	if response.json()['verbose_msg'] == "The requested resource is not among the finished, queued or pending scans":
-		return "Please wait a few minutes for another request, your request has been sent to the queue"
+	try:
+		sha256sum = sha256(fname)
+		url = 'https://www.virustotal.com/vtapi/v2/file/report'
+		params = {'apikey': '6201732de559c1e9c089e897ce858f0df21efb98f417e51c5ec6a08031abcf6e', 'resource': sha256sum}
+		response = requests.get(url, params=params)
+		if response.json()['verbose_msg'] == "The requested resource is not among the finished, queued or pending scans":
+			return "Please wait a few minutes for another request, your request has been sent to the queue"
 
-	if response.json()['positives'] > 0:
-		found = []
-		for attr, value in response.json()['scans'].items():
-			if value['detected'] == True:
-				tmp = []
-				tmp.append(attr)
-				tmp.append(value['result'])
-				found.append(tmp)
-		return response.json()['positives'], response.json()['total'], response.json()['permalink'], found
-	else:
-		return "Virustotal didn't find anything suspicious with this file"
+		if response.json()['positives'] > 0:
+			found = []
+			for attr, value in response.json()['scans'].items():
+				if value['detected'] == True:
+					tmp = []
+					tmp.append(attr)
+					tmp.append(value['result'])
+					found.append(tmp)
+			return response.json()['positives'], response.json()['total'], response.json()['permalink'], found
+		else:
+			return "Virustotal didn't find anything suspicious with this file"
+	except:
+		return "Please wait a few minutes for another request, your request has been sent to the queue"
 
 def hashsum(fname):
 	return [['md5',md5(fname)], ['sha1',sha1(fname)], ['sha256',sha256(fname)]]
